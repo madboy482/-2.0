76d6510ee3925c009df9f79e1e7cf19b998bbaaf
pranavjha2426 <71507799+pranavjha2426@users.noreply.github.com>
2021-01-29 11:38:52 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,7 +146,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                    [InlineKeyboardButton(text="ADD Ultra max TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="none")
+                                                                       url="https://github.com/pranavjha2426/GRANDROBOT")
                                                                                  ]]))
 
     else:
